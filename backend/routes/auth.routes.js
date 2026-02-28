7bbe74da3db2f4f128fc930286ad5c100933a264
ravatech3 <antidroidconcepts@gmail.com>
2026-02-28 15:58:56 +0100

    Removed backend folder from main branch

--- backend/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registerStudent, loginStudent } from '../controller/user.controller.js';
-const router = express.Router();
-
-router.post('/register', registerStudent);
-router.post('/login', loginStudent);
-
-export default router;

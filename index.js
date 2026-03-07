b69465bfb987a47c5e800dc0e6701e6f47c23956
AbdulAzeez Alimi <alimiazeez400@gmail.com>
2026-03-07 11:20:58 +0100

    updating CORS

--- index.js
+++ index.js
@@ -5,26 +5,40 @@ import 'dotenv/config';
 import conncetDB from './src/config/db.js';
 import authroutes from './src/routes/auth.routes.js';
 
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 app.use(morgan('dev'));
 app.use(express.json());
 
+
+app.use(cors({
+  origin: true, 
+  credentials: true
+}));
+
+
 conncetDB();
+
 
 app.get("/", (req, res) => {
   res.send("C-transit server is running");
 });
+<<<<<<< Updated upstream:index.js
 app.use(cors({
   origin: [
     "http://localhost:3000",
     "// Add the frontend URL here"
   ],
 }));
+=======
+>>>>>>> Stashed changes:backend/index.js
 
 app.use('/api/auth', authroutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+

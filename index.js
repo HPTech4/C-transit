89401215ba023a9dbaebdb031f83b4ef6340ac03
ravatech3 <antidroidconcepts@gmail.com>
2026-03-07 16:10:43 +0100

    cors now ready to deploy

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
 import 'dotenv/config';
-import conncetDB from './src/config/db.js';
-import authroutes from './src/routes/auth.routes.js';
+import connectDB from "./src/config/db.js";
+import authRoutes from './src/routes/auth.routes.js';
 
 
 const app = express();
@@ -20,26 +20,22 @@ app.use(cors({
 }));
 
 
-conncetDB();
+connectDB();
 
 
 app.get("/", (req, res) => {
   res.send("C-transit server is running");
 });
-<<<<<<< Updated upstream:index.js
-<<<<<<< Updated upstream:index.js
+
 app.use(cors({
   origin: [
     "http://localhost:3000",
-    "// Add the frontend URL here"
+    "https://c-transit.vercel.app/"
   ],
+  methods: ["GET", "POST", "PUT", "DELETE"]
 }));
-=======
->>>>>>> Stashed changes:backend/index.js
-=======
->>>>>>> Stashed changes:backend/index.js
 
-app.use('/api/auth', authroutes);
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
